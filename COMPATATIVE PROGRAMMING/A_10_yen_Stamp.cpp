6af96e19973b2f967df72f34d0fe30ebec26def2
Jaspal Singh <singhjaspal4951@gmail.com>
2026-04-07 22:24:38 +0530

    Bug fix

--- COMPATATIVE PROGRAMMING/A_10_yen_Stamp.cpp
+++ COMPATATIVE PROGRAMMING/A_10_yen_Stamp.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
-using namespace std ;
-int main (){
-    int x , y ;
-    cin>>x >> y ;
-    
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int ceil(int a,int b){return (a+b-1)/b;}
+
+int main(){
+  int x,y;
+  cin >> x >> y;
+  if(x>=y){cout << "0\n";return 0;}
+  cout << ceil(y-x,10) << '\n';
+  return 0;
 }
